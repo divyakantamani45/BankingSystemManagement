598c9fb15bda3439513bc4c6bb2d8ef720d078b4
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2021-10-22 09:49:41 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
-#include <iostream>
+/* 
+Author:Divya Kantamani
+date created 20-10-2021
+date modified 22-10-2021
+*/
+#include <iostream> // include preprocessors
 #include <fstream>
 using namespace std;
-class Account
+class Account // declaring class
 {
-private:
+private: // visibility is only for class
     int accNo_;
     string name_;
     char type_;
     float balance_;
     string address_;
 
-
-public:
-    int getAccountNum() { return accNo_; }
+public:                                    // visibility is everywhere
+    int getAccountNum() { return accNo_; } // below are accessor functions
     int getBalance() { return balance_; }
     string getName() { return name_; }
     string getAddress() { return address_; }
     void setBalance(int b) { balance_ = b; }
     void takeData();
     void showData();
-    friend std::ostream &operator<<(std::ostream &out, const Account &obj)
+    friend std::ostream &operator<<(std::ostream &out, const Account &obj) //friend funtion to overload << operator
     {
         out << obj.accNo_ << "\n"
             << obj.name_ << "\n"
@@ -28,7 +32,7 @@ public:
             << obj.address_ << std::endl;
         return out;
     }
-    friend std::istream &operator>>(std::istream &in, Account &obj)
+    friend std::istream &operator>>(std::istream &in, Account &obj) //friend funtion to overload >> operator
     {
         in >> obj.accNo_;
         in >> obj.name_;
@@ -38,7 +42,7 @@ public:
         return in;
     }
 };
-void Account::takeData()
+void Account::takeData() //member funtion to take account data
 {
     cout << "Enter Account number : ";
     cin >> accNo_;
@@ -51,7 +55,7 @@ void Account::takeData()
     cout << "Enter address of account holder : ";
     cin >> address_;
 }
-void Account::showData()
+void Account::showData() //member funtion to show account data
 {
     cout << "********Details************\n";
     cout << "Account number : " << accNo_ << "\n";
@@ -60,7 +64,7 @@ void Account::showData()
     cout << "Balance : " << balance_ << "\n";
     cout << "Address : " << address_ << "\n";
 }
-void displayAll()
+void displayAll() // method to display all acounts from file
 {
     string FileName = "bank_accounts.txt";
     ifstream in;
@@ -72,7 +76,7 @@ void displayAll()
     }
     in.close();
 }
-void lastEntry()
+void lastEntry() // method to display last transaction  entry from transaction details file
 {
     string FileName = "transaction_details.txt";
     ifstream in;
@@ -85,7 +89,7 @@ void lastEntry()
     cout << p << " " << balance << " from Account Number : " << accNo << "\n";
     in.close();
 }
-void displayAllTransactions()
+void displayAllTransactions() // display all entries in transaction_details file
 {
     string FileName = "transaction_details.txt";
     ifstream in;
@@ -94,10 +98,11 @@ void displayAllTransactions()
     string p;
     while (in >> accNo >> p >> balance)
     {
-        cout << p <<" "<< abs(balance) << " from Account Number : " << accNo << "\n";
+        cout << p << " " << abs(balance) << " from Account Number : " << accNo << "\n";
     }
     in.close();
 }
+/* returns account object if present in file with given account number else returns empty object*/
 Account displaySpecific(int accNumber)
 {
     string FileName = "bank_accounts.txt";
@@ -117,7 +122,7 @@ Account displaySpecific(int accNumber)
     Account b;
     return b;
 }
-void deleteAccount(int accNumber)
+void deleteAccount(int accNumber) // delete account from file using account number
 {
     string FileName1 = "temp.txt";
     ofstream out;
@@ -144,7 +149,7 @@ void deleteAccount(int accNumber)
     remove("bank_accounts.txt");
     rename("temp.txt", "bank_accounts.txt");
 }
-void add(Account a)
+void add(Account a) // add an account to file
 {
     string FileName = "bank_accounts.txt";
     ofstream out;
@@ -152,6 +157,7 @@ void add(Account a)
     out << a;
     out.close();
 }
+/*update balance in the file for deposit and withdraw for an accout at the same time adding it to transaction_details file*/
 void update(int accNo, int amount)
 {
     string FileName1 = "temp.txt";
@@ -218,18 +224,18 @@ int main()
         cout << "Enter the required option : ";
         int option;
         cin >> option;
-        if (option == 1)
+        if (option == 1) //Add new Account holder
         {
             Account a;
             a.takeData();
             add(a);
             cout << "Account added successfully....\n";
         }
-        else if (option == 2)
+        else if (option == 2) // Display all account holders
         {
             displayAll();
         }
-        else if (option == 3)
+        else if (option == 3) // Display details for the account number
         {
             cout << "\nEnter Account number to search : ";
             int accNo_;
@@ -240,14 +246,14 @@ int main()
             else
                 acc.showData();
         }
-        else if (option == 4)
+        else if (option == 4) // Delete Account
         {
             cout << "\nEnter Account number to delete : ";
             int accNo_;
             cin >> accNo_;
             deleteAccount(accNo_);
         }
-        else if (option == 5)
+        else if (option == 5) // Deposit in a Account
         {
             cout << "\nEnter Account number to deposit : ";
             int accNo_, amount;
@@ -262,7 +268,7 @@ int main()
                 update(accNo_, amount);
             }
         }
-        else if (option == 6)
+        else if (option == 6) // Withdraw in a Account
         {
             cout << "\nEnter Account number to withdraw : ";
             int accNo_, amount;
@@ -277,7 +283,7 @@ int main()
                 update(accNo_, (-1) * amount);
             }
         }
-        else if (option == 7)
+        else if (option == 7) // Check if an account Exists or not
         {
             cout << "Enter Account number to search : ";
             int accNo_;
@@ -288,7 +294,7 @@ int main()
             else
                 cout << "Account number exists in the database\n";
         }
-        else if (option == 8)
+        else if (option == 8) // Retrieve name of Account holder
         {
             cout << "\nEnter Account number to search : ";
             int accNo_;
@@ -299,7 +305,7 @@ int main()
             else
                 cout << "Name of Account holder : " << acc.getName() << "\n";
         }
-        else if (option == 9)
+        else if (option == 9) // Retrieve address of Account holder
         {
             cout << "\nEnter Account number to search : ";
             int accNo_;
@@ -310,7 +316,7 @@ int main()
             else
                 cout << "Address of Account holder : " << acc.getAddress() << "\n";
         }
-        else if (option == 10)
+        else if (option == 10) // Retrieve balance of Account holder
         {
             cout << "\nEnter Account number to search : ";
             int accNo_;
@@ -321,11 +327,11 @@ int main()
             else
                 cout << "Balance of Account holder : " << acc.getBalance() << "\n";
         }
-        else if (option == 11)
+        else if (option == 11) // Display the last Entry
         {
             lastEntry();
         }
-        else if (option == 12)
+        else if (option == 12) //Display Transaction details with account number
         {
             displayAllTransactions();
         }
@@ -337,8 +343,8 @@ int main()
         {
             cout << "\nInvalid operation!!\n";
         }
-        cout<<"Enter 1 to continue or enter 0 to exit : ";
-        cin>>check;
+        cout << "Enter 1 to continue or enter 0 to exit : ";
+        cin >> check;
     } while (check);
 
     return 0;
